c1dbf61291dfd8ea07bdf183e74fbfb805d35a3c
Pratik Shrestha (Sudapey) <60513331+pratiksudapey@users.noreply.github.com>
2024-04-06 14:13:52 +0545

    frontend for ome page setup completed

---
+++ resources/views/layouts/partial/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="bg-blue-950">
+  <div class="p-8 xl:ml-auto xl:mr-auto xl:w-[1280px]">
+    <div class="text-center text-[16px] font-medium tracking-[4px] text-gray-300 md:text-[28px]">
+      © Lakai IT Pvt. Ltd. {{ date('Y') }}, All Right Reserved
+    </div>
+  </div>
+</footer>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\BlogController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [BlogController::class, 'index'])->name('blog.index');
+Route::resource('blogs', BlogController::class)->only('show');
